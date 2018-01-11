17dd96c6cc21db9779eb002d1e69c3ab12770e98
DESKTOP-8NP98OJ\JEONGWOOJIN <wjddnwls918@naver.com>
2018-01-11 17:44:09 +0900

    no message

---
+++ 라즈베리/project.js
@@ -0,0 +1,103 @@
+var express = require('express')
+
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+var mysql = require('mysql');
+var connection = mysql.createConnection(
+		{
+host : 'localhost',
+user : 'wangtou',
+password :'fkaus918',
+database : 'test'
+});
+
+connection.connect();
+
+/*
+var query = connection.query('select * from `user`',function(err,result)
+		{
+			console.log(result);
+			conncetion.end();
+			})
+*/
+
+var SerialPort = require('serialport');
+
+
+app.get('/',function(req,res)
+		{
+		
+		res.send("Hello, World")
+		})
+app.listen(8888,function()
+		{
+		console.log("Server starting with 8888")
+		})
+
+
+
+//SerialPort.read(30);
+var port = new SerialPort('/dev/ttyACM0',{
+	baudRate: 115200
+});
+	sensorVal = 0;
+
+
+
+port.on('open',function()
+	{
+	console.log('serial port OPEN');
+
+	});
+
+port.on('error', function(err)
+	{
+	console.log('Error: ', err.message);
+	});
+/*
+port.on('data',function(data)
+	{
+	//console.log(data.temp +", "+data.humi+", "+data.button);\
+	console.log(data.length);
+	if(data.length >= 21 )
+	{
+		console.log('Read Data: '+data);
+		
+		var insert = connection.query('insert into sensordata set ?'
+				,sensordata,function(error,results,fields)
+				{
+				if(error) throw error;
+				console.log(results.insertId);
+				
+			//	connection.end();
+				
+				});
+
+	}
+
+	}
+);
+*/
+
+//read data from Arduino
+const Readline = SerialPort.parsers.Readline;
+const parser = port.pipe(new Readline({ delimiter: '\n'  }));
+parser.on('data', test);
+
+function test(data)
+{
+	console.log(data);
+	console.log("success ??");
+	console.log(typeof(data));
+
+	var sensordata = String(data).split(',');
+
+	for( var i =0; i<sensordata.length; i++)
+	{
+		console.log(sensordata[i]);
+	
+	}
+}
---
+++ 아두이노/project.ino
@@ -0,0 +1,144 @@
+//#include <DHT11.h>
+#include <DHT.h>
+#include <WiFi.h>
+#include "MQ135.h"
+#include <ArduinoJson.h>
+#define DHTPIN 3
+#define DHTTYPE DHT22
+#define BUZZER 10
+#define RLOAD 10.0
+
+#define RZERO 76.63
+
+#define PARA 116.6020682
+#define PARB 2.769034858
+
+#define ANALOGPIN A0
+//#define RZERO 206.85
+MQ135 gasSensor = MQ135(ANALOGPIN);
+
+const int beepFrequency = 900;
+const int beepDuration = 500;
+
+int flag = 0;
+int sw=2;
+int flame = 4;
+int state = 0;
+int led = 7;
+int humSensor = 5;
+
+DHT dht(DHTPIN,DHTTYPE);
+int tones[] = {261,294,330,349,392,440,494,523};
+
+//DHT11 dht11(temhu);
+
+void setup()
+{
+  
+  dht.begin();
+  pinMode(sw,INPUT_PULLUP);
+  pinMode(DHTPIN,INPUT);
+  pinMode(flame, INPUT);
+  pinMode(led , OUTPUT);
+  pinMode(humSensor, INPUT);
+  //float rzero = gasSensor.getRZero();
+  //delay(3000);
+  //Serial.print("MQ135 RZERO Calibration Value : ");
+  //Serial.println(rzero);
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  state = digitalRead(flame);
+  
+  digitalWrite(led,LOW);
+  
+  if (state ==0)
+  {
+    //Serial.println("ON");
+    digitalWrite(led,HIGH);
+    delay(100);
+  }
+  else
+  {
+    //Serial.println("OFF");
+    digitalWrite(led, LOW);
+  }
+  
+  //delay(100);
+  
+  int humState = digitalRead(humSensor);
+  //Serial.println(humstate);
+  if(humState == HIGH)
+  {
+    digitalWrite(led,HIGH);
+    delay(500);
+    digitalWrite(led,LOW);
+    delay(500);
+  }
+    
+  
+  
+  int buttonState = digitalRead(sw);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+ 
+ 
+  float correctedRZero = gasSensor.getCorrectedRZero(t,h);
+  float resistance = gasSensor.getResistance();
+  float ppm = gasSensor.getPPM();
+  float correctedPPM = gasSensor.getCorrectedPPM(t,h);
+ 
+  // Serial.print("ppm : ");
+  // Serial.println(ppm *1000);
+ 
+  
+ 
+  //  Serial.print(t);
+  //  Serial.print(" ");
+    //delay(1000);
+  //  Serial.println(h);
+    //Serial.print(" ");
+    //delay(1000);
+
+  buzzerAct(buttonState);  
+  
+  Serial.print(t);
+  Serial.print(',');
+  Serial.print(h);
+  Serial.print(',');
+  Serial.print(ppm*1000);
+  Serial.print(',');
+  //flame sensor
+  Serial.print(state);
+  Serial.print(',');
+  Serial.print(humState);
+  Serial.print('\n');
+  
+  /*
+   String jsondata ="";
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject &root = jsonBuffer.createObject();
+  root["temp"] = t;
+  root["humi"] = h;
+  root["ppm"] = ppm *1000;
+  root["flameState"] = state;
+  root["humState"] = humState;
+  root.printTo(jsondata);
+  Serial.println(jsondata);
+ */
+  
+  delay(3000);
+}
+  
+void buzzerAct(int buttonState)
+{
+  if( buttonState == 0)
+  {
+    tone(BUZZER, beepFrequency, beepDuration);
+  }
+}
+  
+  
+  
