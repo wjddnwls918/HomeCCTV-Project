b77b93c641e3a9aff235f06d2c1fc642fe9485a1
WooJin Jeong <wjddnwls918@naver.com>
2018-01-31 14:31:08 +0900

    Update Register.php

--- rasberry/Register.php
+++ rasberry/Register.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost","wangtou","fkaus918","test");
+$con = mysqli_connect("localhost","#","#","test");
 
 $userID = $_POST["userID"];
 $userPW = $_POST["userPW"];
